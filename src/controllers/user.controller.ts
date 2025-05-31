29b53f97189172aafc7460853fba5a3a2df813b7
oseijoel6111 <oseijoel6111@gmail.com>
2025-05-31 07:57:56 -0500

    feat: refactor password hashing logic and update signUp method to handle errors

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,16 +1,19 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { findUserByEmail } from '../services'
 import { successResponse } from '../utils/response.util'
+import {generateHash} from '../utils/password.util'
 
-export const signUp = (req: Request, res: Response) => {
-
+export const signUp = (req: Request, res: Response, next:NextFunction) => {
+    try{
     //    determine if user already exist
     const existingUser = findUserByEmail(req.body.email);
-    if (!existingUser) return successResponse(res, 400, 'User already exists', { email: 'Email already registered' });
+    if (!existingUser)  successResponse(res, 400, 'User already exists', { email: 'Email already registered' });
 
     // hash password
-    
+    const hashPassword = generateHash(req.body.password)
+    req.body.password = hashPassword;
 
+    
     // save user to database
 
     // send verification email
@@ -23,6 +26,10 @@ export const signUp = (req: Request, res: Response) => {
         }
     }
     res.json(userResponse);
+
+}catch (error) {
+    next(error);
+    }
 }
 
 export const login = (req: Request, res: Response) => {
